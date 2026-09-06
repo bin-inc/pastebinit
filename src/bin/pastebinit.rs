236b155ac9c4574f85b51925679b2689c5ac06be
Hardy Nguyen <maw.signup@gmail.com>
2026-09-06 18:09:11 +0700

    feat: add compatible pastebinit CLI parsing (#3)

--- src/bin/pastebinit.rs
+++ src/bin/pastebinit.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use pastebinit::cli::{CliOutcome, HelpDefaults, parse_cli};
+
+fn main() {
+    let defaults = HelpDefaults::reference_defaults("bpa.st", "maw");
+    match parse_cli(std::env::args_os(), &defaults) {
+        Ok(CliOutcome::Help(help)) | Ok(CliOutcome::Version(help)) => print!("{help}"),
+        Ok(CliOutcome::Run(_)) => {}
+        Err(error) => {
+            eprintln!("{}", error.message());
+            std::process::exit(error.exit_code());
+        }
+    }
+}
